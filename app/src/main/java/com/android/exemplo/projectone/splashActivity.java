63838b284e8d1d8205d7e45ebc47a35079cf3ca1
carvbapt <carvbapt@gmail.com>
2015-06-02 17:05:36 +0100

    S@C - Pastas Drawables

--- app/src/main/java/com/android/exemplo/projectone/splashActivity.java
+++ app/src/main/java/com/android/exemplo/projectone/splashActivity.java
@@ -12,6 +12,8 @@ public class splashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
     }
 
     @Override
