843dd3216b8ab380a1273ee868dfef8b58235ded
carvbapt <carvbapt@gmail.com>
2015-06-02 17:55:49 +0100

    S@C - Update splash, dashboard.          - Remoção de menubar splash

--- app/src/main/java/com/android/exemplo/projectone/DashBoardActivity.java
+++ app/src/main/java/com/android/exemplo/projectone/DashBoardActivity.java
@@ -1,17 +1,32 @@
 package com.android.exemplo.projectone;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class DashBoardActivity extends Activity {
+
+    Button BTtoagenda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+        BTtoagenda=(Button)findViewById(R.id.BTtoagenda);
+
+        BTtoagenda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(DashBoardActivity.this,AgendaActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/android/exemplo/projectone/splashActivity.java
+++ app/src/main/java/com/android/exemplo/projectone/splashActivity.java
@@ -1,17 +1,33 @@
 package com.android.exemplo.projectone;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class splashActivity extends Activity {
+
+    Button BTtodash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        BTtodash=(Button)findViewById(R.id.BTtodash);
+
+        BTtodash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(splashActivity.this,DashBoardActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }
