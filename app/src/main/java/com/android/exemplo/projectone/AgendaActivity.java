1c938e4d45dd43cc013472df187e6f6d15d9159e
carvbapt <carvbapt@gmail.com>
2015-06-04 19:02:12 +0100

    TGB - files modificados: -agendafragment.java; -agendaactivity; -activity_agenda.xml; -fragment_agenda.xml; -styles.xml; -styles.xml v21; AndroidManifest.xml; -build.gradle;

--- app/src/main/java/com/android/exemplo/projectone/AgendaActivity.java
+++ app/src/main/java/com/android/exemplo/projectone/AgendaActivity.java
@@ -3,15 +3,22 @@ package com.android.exemplo.projectone;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.ActionBar;
 
+import com.android.exemplo.projectone.agenda.agendaFragment;
 import com.roomorama.caldroid.CaldroidFragment;
 import com.roomorama.caldroid.CaldroidListener;
 
@@ -20,152 +27,86 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
-@SuppressLint("SimpleDateFormat")
-public class AgendaActivity extends FragmentActivity {
-    private boolean undo = false;
-    private CaldroidFragment caldroidFragment;
-    private CaldroidFragment dialogCaldroidFragment;
 
-    private void setCustomResourceForDates() {
-        Calendar cal = Calendar.getInstance();
+public class AgendaActivity extends AppCompatActivity {
 
-        // Min date is last 7 days
-        cal.add(Calendar.DATE, -7);
-        Date blueDate = cal.getTime();
-
-        // Max date is next 7 days
-        cal = Calendar.getInstance();
-        cal.add(Calendar.DATE, 7);
-        Date greenDate = cal.getTime();
-
-        // Termino de Contratos
-        cal = Calendar.getInstance();
-        cal.add(Calendar.DATE, 10);
-        Date redDate = cal.getTime();
-
-
-
-        if (caldroidFragment != null) {
-            caldroidFragment.setBackgroundResourceForDate(R.color.caldroid_holo_blue_light, blueDate);
-            caldroidFragment.setTextColorForDate(R.color.caldroid_sky_blue, blueDate);
-
-            caldroidFragment.setBackgroundResourceForDate(R.color.color_green_400, greenDate);
-            caldroidFragment.setTextColorForDate(R.color.caldroid_white, greenDate);
-
-            caldroidFragment.setBackgroundResourceForDate(R.color.color_deep_orange_500, redDate);
-            caldroidFragment.setTextColorForDate(R.color.caldroid_white, redDate);
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agenda);
 
-        final SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy");
+        agenda();
+    }
 
-        // Setup caldroid fragment
-        // **** If you want normal CaldroidFragment, use below line ****
-        caldroidFragment = new CaldroidFragment();
-
-        // //////////////////////////////////////////////////////////////////////
-        // **** This is to show customized fragment. If you want customized
-        // version, uncomment below line ****
-//		 caldroidFragment = new CaldroidSampleCustomFragment();
-
-        // Setup arguments
-
-        // If Activity is created after rotation
-        if (savedInstanceState != null) {
-            caldroidFragment.restoreStatesFromKey(savedInstanceState,
-                    "CALDROID_SAVED_STATE");
-        }
-        // If activity is created from fresh
-        else {
-            Bundle args = new Bundle();
-            Calendar cal = Calendar.getInstance();
-            args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
-            args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
-            args.putBoolean(CaldroidFragment.ENABLE_SWIPE, true);
-            args.putBoolean(CaldroidFragment.SIX_WEEKS_IN_CALENDAR, true);
-
-            // Uncomment this to customize startDayOfWeek
-            // args.putInt(CaldroidFragment.START_DAY_OF_WEEK,
-            // CaldroidFragment.TUESDAY); // Tuesday
-
-            // Uncomment this line to use Caldroid in compact mode
-            // args.putBoolean(CaldroidFragment.SQUARE_TEXT_VIEW_CELL, false);
-
-            // Uncomment this line to use dark theme
-            args.putInt(CaldroidFragment.THEME_RESOURCE, R.style.CaldroidDefaultDark);
-
-            caldroidFragment.setArguments(args);
-        }
-
-        setCustomResourceForDates();
-
-        // Attach to the activity
-        FragmentTransaction t = getSupportFragmentManager().beginTransaction();
-        t.replace(R.id.calendar1, caldroidFragment);
-        t.commit();
-
-        // Setup listener
-        final CaldroidListener listener = new CaldroidListener() {
-
-            @Override
-            public void onSelectDate(Date date, View view) {
-                Toast.makeText(getApplicationContext(), formatter.format(date),
-                        Toast.LENGTH_SHORT).show();
-
-            }
-
-            @Override
-            public void onChangeMonth(int month, int year) {
-                String text = "month: " + month + " year: " + year;
-                Toast.makeText(getApplicationContext(), text,
-                        Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onLongClickDate(Date date, View view) {
-                Toast.makeText(getApplicationContext(),
-                        "Long click " + formatter.format(date),
-                        Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onCaldroidViewCreated() {
-                if (caldroidFragment.getLeftArrowButton() != null) {
-                    Toast.makeText(getApplicationContext(),
-                            "Caldroid view is created", Toast.LENGTH_SHORT)
-                            .show();
-                }
-            }
-
-        };
-
-        // Setup Caldroid
-        caldroidFragment.setCaldroidListener(listener);
-
-        final TextView textView = (TextView) findViewById(R.id.textview);
-
+    public void agenda (){
+        ViewPager pager = (ViewPager) findViewById(R.id.viewPagerAgenda);
+        pager.setAdapter(new MyPagerAdapterSubZones(getSupportFragmentManager()));
     }
 
     /**
      * Save current states of the Caldroid here
      */
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        // TODO Auto-generated method stub
-        super.onSaveInstanceState(outState);
+//    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        // TODO Auto-generated method stub
+//        super.onSaveInstanceState(outState);
+//
+//        if (caldroidFragment != null) {
+//            caldroidFragment.saveStatesToKey(outState, "CALDROID_SAVED_STATE");
+//        }
+//
+//        if (dialogCaldroidFragment != null) {
+//            dialogCaldroidFragment.saveStatesToKey(outState,
+//                    "DIALOG_CALDROID_SAVED_STATE");
+//        }
+//    }
 
-        if (caldroidFragment != null) {
-            caldroidFragment.saveStatesToKey(outState, "CALDROID_SAVED_STATE");
+
+
+    private class MyPagerAdapterSubZones extends FragmentPagerAdapter {
+
+        public MyPagerAdapterSubZones(FragmentManager fm) {
+            super(fm);
         }
 
-        if (dialogCaldroidFragment != null) {
-            dialogCaldroidFragment.saveStatesToKey(outState,
-                    "DIALOG_CALDROID_SAVED_STATE");
+        @Override
+        public Fragment getItem(int pos) {
+            switch(pos) {
+
+                case 0: return agendaFragment.newInstance();
+//                case 1: return SecondFragment.newInstance("SecondFragment, Instance 1");
+//                case 2: return ThirdFragment.newInstance("ThirdFragment, Instance 1");
+//                case 3: return ThirdFragment.newInstance("ThirdFragment, Instance 2");
+//                case 4: return ThirdFragment.newInstance("ThirdFragment, Instance 3");
+                default:
+//                    if local == nr faz para essa zona
+//                    if ((local==4)||(local==5)) // If Arm left or Arm righ
+//                        return AlarmSubZonFragment_Braco.newInstance(local, text_localBody);
+//                    else {
+//                        if ((local == 0) || (local == 1)) // If left abdomen or right abdomen
+//                            return AlarmSubZonFragment_Abdomen.newInstance(local, text_localBody);
+//                        else{
+//                            if ((local == 2) || (local == 3)) // If left coxa or right coxa
+//                                return AlarmSubZonFragment_Coxa.newInstance(local, text_localBody);
+//                            else{
+//                                if ((local == 6) || (local == 7)) // If left nadega or right nadega
+//                                    return AlarmSubZonFragment_Nadega.newInstance(local, text_localBody);
+//                                else
+                    return null;
+//                            }
+//                        }
+//
+//                    }
+            }
+        }
+
+        @Override
+        public int getCount() {
+//            if (local!=(-1))
+//                return 2;
+//            else
+            return 1;
         }
     }
 
---
+++ app/src/main/java/com/android/exemplo/projectone/agenda/agendaFragment.java
@@ -0,0 +1,250 @@
+package com.android.exemplo.projectone.agenda;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.exemplo.projectone.R;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+
+import android.support.v4.app.FragmentTransaction;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.roomorama.caldroid.CaldroidFragment;
+import com.roomorama.caldroid.CaldroidListener;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+
+
+@SuppressLint("SimpleDateFormat")
+public class agendaFragment extends android.support.v4.app.Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private boolean undo = false;
+    private CaldroidFragment caldroidFragment;
+    private CaldroidFragment dialogCaldroidFragment;
+
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment agendaFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static agendaFragment newInstance(String param1, String param2) {
+        agendaFragment fragment = new agendaFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public agendaFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    public static agendaFragment newInstance() {
+
+        agendaFragment f = new agendaFragment();
+        Bundle b = new Bundle();
+//        b.putString("msg", text);
+//        b.putInt("zona", local);
+
+
+
+        f.setArguments(b);
+
+        return f;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_agenda, container, false);
+
+        final SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy");
+
+        // Setup caldroid fragment
+        // **** If you want normal CaldroidFragment, use below line ****
+        caldroidFragment = new CaldroidFragment();
+
+        // //////////////////////////////////////////////////////////////////////
+        // **** This is to show customized fragment. If you want customized
+        // version, uncomment below line ****
+//		 caldroidFragment = new CaldroidSampleCustomFragment();
+
+        // Setup arguments
+
+        // If Activity is created after rotation
+        if (savedInstanceState != null) {
+            caldroidFragment.restoreStatesFromKey(savedInstanceState,
+                    "CALDROID_SAVED_STATE");
+        }
+        // If activity is created from fresh
+        else {
+            Bundle args = new Bundle();
+            Calendar cal = Calendar.getInstance();
+            args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+            args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+            args.putBoolean(CaldroidFragment.ENABLE_SWIPE, true);
+            args.putBoolean(CaldroidFragment.SIX_WEEKS_IN_CALENDAR, true);
+
+            // Uncomment this to customize startDayOfWeek
+            // args.putInt(CaldroidFragment.START_DAY_OF_WEEK,
+            // CaldroidFragment.TUESDAY); // Tuesday
+
+            // Uncomment this line to use Caldroid in compact mode
+            args.putBoolean(CaldroidFragment.SQUARE_TEXT_VIEW_CELL, false);
+
+            // Uncomment this line to use dark theme
+//            args.putInt(CaldroidFragment.THEME_RESOURCE, R.style.CaldroidDefaultDark);
+
+            caldroidFragment.setArguments(args);
+        }
+
+        setCustomResourceForDates();
+
+
+        // Attach to the activity
+        FragmentTransaction t = getActivity()
+                .getSupportFragmentManager().beginTransaction();//getSupportFragmentManager().beginTransaction();
+        t.replace(R.id.calendar1, caldroidFragment);
+        t.commit();
+
+        // Setup listener
+        final CaldroidListener listener = new CaldroidListener() {
+
+            @Override
+            public void onSelectDate(Date date, View view) {
+                Toast.makeText(getActivity().getBaseContext(), formatter.format(date),
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onChangeMonth(int month, int year) {
+                String text = "month: " + month + " year: " + year;
+                Toast.makeText(getActivity().getBaseContext(), text,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClickDate(Date date, View view) {
+                Toast.makeText(getActivity().getBaseContext(),
+                        "Long click " + formatter.format(date),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCaldroidViewCreated() {
+                if (caldroidFragment.getLeftArrowButton() != null) {
+                    Toast.makeText(getActivity().getBaseContext(),
+                            "Caldroid view is created", Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+
+        };
+
+        // Setup Caldroid
+        caldroidFragment.setCaldroidListener(listener);
+
+        final TextView textView = (TextView) v.findViewById(R.id.textview);
+
+        return v;
+    }
+
+    private void setCustomResourceForDates() {
+        Calendar cal = Calendar.getInstance();
+
+        // Min date is last 7 days
+        cal.add(Calendar.DATE, -7);
+        Date blueDate = cal.getTime();
+
+        // Max date is next 7 days
+        cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 7);
+        Date greenDate = cal.getTime();
+
+        // Termino de Contratos
+        cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 10);
+        Date redDate = cal.getTime();
+
+
+
+        if (caldroidFragment != null) {
+            caldroidFragment.setBackgroundResourceForDate(R.color.caldroid_holo_blue_light, blueDate);
+            caldroidFragment.setTextColorForDate(R.color.caldroid_sky_blue, blueDate);
+
+            caldroidFragment.setBackgroundResourceForDate(R.color.color_green_400, greenDate);
+            caldroidFragment.setTextColorForDate(R.color.caldroid_white, greenDate);
+
+            caldroidFragment.setBackgroundResourceForDate(R.color.color_deep_orange_500, redDate);
+            caldroidFragment.setTextColorForDate(R.color.caldroid_white, redDate);
+        }
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
